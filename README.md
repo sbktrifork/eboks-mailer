e3cc2526328c59cabebd555636fb60ad30128754
Christian Panton <christian@panton.org>
2015-10-01 19:27:37 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+e-Boks mailer
+=============
+
+Fetch unread "e-Boks" messages and mail them to yourself. 
+Useful in a closed ecosystem, where you control your own mail server.
+
+Usage:
+     eboks-mailer.py /path/to/config.yaml
+
+You need to configure this with your CPR#, password and activation key.
+
+Requirements:
+
+- requests
+- BeautifulSoup (bs4)
+
+Integration with Sieve filters
+------------------------------
+
+An example usage with Dovecot
+
+
+Example sieve filter:
+
+    require ["vnd.dovecot.execute"];
+
+    if address :matches "From" "besked@advisering.e-boks.dk" {
+        execute "eboks-mailer-trigger /path/to/config.yaml";
+        stop;
+    }
+
+
---
+++ eboks-mailer.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+
+# -*- coding: utf-8 -*-
+
+import string
+import re
+import unicodedata
+import mimetypes 
+import smtplib
+import sys
+import yaml
+
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEBase import MIMEBase
+from email import Encoders
+
+import requests
+from bs4 import BeautifulSoup
+
+def slugify(text):
+    text = unicodedata.normalize('NFKD', text).lower()
+    return re.sub(r'\W+', '_', text)
+
+
+config = yaml.load(file(sys.argv[1], "r").read())
+
+EMAIL_TO = config["to"]
+EMAIL_FROM = config["from"]
+
+LOGIN_URL = "https://m.e-boks.dk/logon.aspx"
+INBOX_URL = "https://m.e-boks.dk/inbox.aspx"
+DOC_URL = "https://m.e-boks.dk/inbox_document.aspx"
+DOCVIEW_URL = "https://m.e-boks.dk/%s"
+
+HEADERS = {'User-agent': 'Mozilla/5.0 (Linux; U;) Mobile'}
+
+s = requests.Session()
+s.headers.update(HEADERS)
+
+print "[*] Fetching tokens from login page"
+
+r = s.get(LOGIN_URL)
+r.raise_for_status()
+
+soup = BeautifulSoup(r.content)
+viewstate = soup.select("#__VIEWSTATE")[0]['value']
+eventvalidation = soup.select("#__EVENTVALIDATION")[0]['value']
+
+form = {
+    "__EVENTTARGET": "lnkOk",
+    "__EVENTARGUMENT": "",
+    "__VIEWSTATE": viewstate,
+    "__VIEWSTATEENCRYPTED": "",
+    "__EVENTVALIDATION": eventvalidation,
+    "Identity": config["cpr"],
+    "Password": config["password"],
+    "ActivationCode": config["activation"]
+}
+
+print "[*] Sending login request"
+
+r = s.post(LOGIN_URL, data=form)
+r.raise_for_status()
+
+if "notification warning" in r.content:
+    print "[-] Login FAILED"
+
+print "[+] Login OK"
+
+
+r = s.get(INBOX_URL)
+r.raise_for_status()
+
+soup = BeautifulSoup(r.content)
+msgs = soup.select("#messages_form")[0]
+
+auth = msgs.select("input[name=auth]")[0]["value"]
+fuid = msgs.select("input[name=fuid]")[0]["value"]
+
+for msg in msgs.select("li"):
+    
+    unread = len(msg.select(".item_unread")) == 1
+
+    if unread:
+
+        msgid = msg.a["href"].split("'")[1]
+        date = msg.select("span.recieved")[0].text
+        sender = msg.select("span.sender")[0].text
+        subject = msg.select("span.title")[0].text
+
+        print "[*] Getting documents:", date, repr(sender), repr(subject)
+
+        form = {"target": msgid, "auth": auth, "fuid": fuid}
+        r = s.post(DOC_URL, data=form)
+        r.raise_for_status()
+
+        soup = BeautifulSoup(r.content)
+
+        msg = MIMEMultipart()
+        msg['Subject'] = subject 
+        msg['From'] = config["from"] % sender
+        msg['To'] = config["to"]
+      
+        for el in soup.select("form li a"):
+
+            url = el["href"]
+            title = el["title"]
+            filetype = el.select("span.info")[0].text[1:].split(",", 1)[0].lower()
+            filename = slugify(unicode(title)) + "." + filetype
+            mime = mimetypes.guess_type(filename)[0]
+
+            print "[*] Downloading attachment:", filename
+
+            r = s.get(DOCVIEW_URL % url)
+            r.raise_for_status()
+            
+            part = MIMEBase(*mime.split("/"))
+            part.set_payload(r.content)
+            Encoders.encode_base64(part)
+            part.add_header('Content-Disposition', 'attachment; filename="%s"' % filename)
+            msg.attach(part)
+
+        server = smtplib.SMTP(config["server"])
+        server.sendmail(config["from"] % sender, config["to"], msg.as_string())
